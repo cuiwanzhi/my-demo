10d2d0819209f6136aaea0d5f8e04d286019fb6d
cuiwanzhi <2368314078@qq.com>
2022-04-09 13:23:35 +0800

    日常刷题

--- typescript/vscode-debug-without-compiling/src/nowCode/DailyCode_22_03_21.ts
+++ typescript/vscode-debug-without-compiling/src/nowCode/DailyCode_22_03_21.ts
@@ -5,6 +5,7 @@
  * @FilePath: \vscode-debug-without-compiling\src\nowCode\DailyCode_22_03_21.ts
  */
 
+import { InversePairs } from "./DailyCode_22_03_23";
 import { ListNode } from "./NowCoder_2022_02_28";
 
 // BM14 链表的奇偶重排
@@ -181,4 +182,5 @@ export function InversePairs(data: number[]): number {
     mergeSort(data);
     
     return count % 1000000007;
-}
+}debugger;
+InversePairs([1,2,3,4,5,6,7,0]);
